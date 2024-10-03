1ca99a5058b9444f0385886b748eeba0e0e9d09b
tuk60682 <kobayashi.shaw.e@gmail.com>
2024-10-03 12:10:15 -0400

    Added a Launcher object using the registerForActivityResult function. Added a launcher. Launch call in the setOnClickListener.

--- app/src/main/java/edu/temple/activities/DisplayActivity.kt
+++ app/src/main/java/edu/temple/activities/DisplayActivity.kt
@@ -1,9 +1,15 @@
 package edu.temple.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
 import android.widget.Button
 import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
+
+
 
 class DisplayActivity : AppCompatActivity() {
 
@@ -14,6 +20,16 @@ class DisplayActivity : AppCompatActivity() {
     private lateinit var lyricsDisplayTextView: TextView
     private lateinit var textSizeSelectorButton: Button
 
+    lateinit var resultTextView: TextView
+
+    val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+        if(it.resultCode == RESULT_OK){
+            it.data?.apply{
+                resultTextView.text = getStringExtra(RESULT_KEY)
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_display)
@@ -21,5 +37,12 @@ class DisplayActivity : AppCompatActivity() {
         lyricsDisplayTextView = findViewById(R.id.lyricsDisplayTextView)
         textSizeSelectorButton = findViewById(R.id.textSizeSelectorButton)
 
+        textSizeSelectorButton.setOnClickListener(object: OnClickListener{
+            override fun onClick(p0: View?) {
+                val textSizeIntent = Intent(this@DisplayActivity,TextSizeActivity::class.java)
+                launcher.launch(textSizeIntent)
+            }
+
+        })
     }
 }
--- app/src/main/java/edu/temple/activities/TextSizeActivity.kt
+++ app/src/main/java/edu/temple/activities/TextSizeActivity.kt
@@ -1,5 +1,6 @@
 package edu.temple.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
